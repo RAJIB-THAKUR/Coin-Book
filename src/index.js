08c4d27eb7aeb4e7753b4a3897d966d0333bb18a
xyz <rajsharma1234@gmail.com>
2023-04-07 21:05:26 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import Log from './components/Log';
+import Sign from './components/Sign';
+import { BrowserRouter, Routes, Route,Navigate, HashRouter } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Main from './components/Main';
+import Yearstat from './components/Yearstat';
+import Monthstat from './components/Monthstat';
+import History from './components/History';
+import Profile from './components/Profile';
+import Passwordchange from './components/Passwordchange';
+import Otpgen from './components/Otpgen';
+import Otpverify from './components/Otpvrify';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+
+      <Routes>
+        <Route element={<App/>}>
+      <Route path="/" element={<Home />}/>
+       <Route path="main" element={<Main />}/>
+        <Route path="login" element={<Log />}/>
+        <Route path="sign" element={<Sign />} />
+        <Route path="about" element={<About />} />
+        <Route path="year" element={<Yearstat />} />
+        <Route path="month" element={<Monthstat />} />
+        <Route path="history" element={<History />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="passchange" element={<Passwordchange />} />
+        <Route path="otpgen" element={<Otpgen />} />
+        <Route path="otpver" element={<Otpverify />} />
+        </Route>
+      </Routes>
+
+    </HashRouter>
   </React.StrictMode>
 );
 
